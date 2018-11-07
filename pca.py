8a520dd32ddf3dd594a3e9a4fbf96a4100c3046d
Bruno Bacelar Abe <messengerabe@hotmail.com>
2018-11-07 14:56:39 -0200

    PCA - Done
    PCA pronto + relatório

--- pca.py
+++ pca.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from numpy.linalg import eig
 
 from matplotlib import pyplot as plt
@@ -7,31 +8,27 @@ import numpy as np
 
 
 #0: setosa, 1: versicolor, 2: Virginica
+
+#Usamos a função na biblioteca pronta para validar os resultados, pode ser ignorado
 def biblePCA():
 	iris = load_iris()
 
 	#x = matriz, y = classe
 	x, y = iris.data, iris.target 
-	print("PRONTO")
-	pca = PCA(1)
+	pca = PCA(2)
 	pca.fit(x)
 	B = pca.transform(x)
-	print(B)
-
-def plotPCA(y, x, newMatrix):
-	"""feature_vector = x.dot(newMatrix)
-	print (feature_vector.T)
-	plt.scatter(feature_vector[0], feature_vector[1], color='r', marker='x')
-	plt.show()"""
-	plt.plot([1, 2, 3, 4])
-	plt.ylabel('some numbers')
-	plt.show()
 
 def myPCA():
 	iris = load_iris()
 
 	#x = matriz, y = classe
-	x, y = iris.data, iris.target 	
+	x, y = iris.data, iris.target 		
+
+	#print(x)
+
+	#Normaliza
+	x = StandardScaler().fit_transform(x)
 	
 	#Matriz de covariança
 	cov = np.cov(x.T)
@@ -46,9 +43,8 @@ def myPCA():
 
 	#Escolhe os dois primeiros(são os que mais "ajudam") e cria a matriz nova
 	newMatrix = np.hstack((pairs[0][1].reshape(4, 1), pairs[1][1].reshape(4, 1)))
-	print(newMatrix)
-
-	#plotPCA(y, x, newMatrix)
+	newSamples = x.dot(newMatrix)
+	print("\n", newSamples)
 
 #biblePCA()
 myPCA()
